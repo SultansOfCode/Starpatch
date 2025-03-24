15b2d08a814e46995901761f32b3268f4e9331e3
SultansOfCode <sultans.of.code@gmail.com>
2025-03-24 18:15:01 -0300

    Adding header stuff + Ability to unpatch files

--- run.bat
+++ run.bat
@@ -3,12 +3,16 @@ zig build
 
 del resources\smaller2bigger_patch.sp 2>NUL
 del resources\smaller2bigger_patched.txt 2>NUL
+del resources\smaller2bigger_unpatched.txt 2>NUL
 
 zig-out\bin\Starpatch.exe create resources\smaller.txt resources\bigger.txt resources\smaller2bigger_patch.sp
 zig-out\bin\Starpatch.exe patch resources\smaller.txt resources\smaller2bigger_patch.sp resources\smaller2bigger_patched.txt
+zig-out\bin\Starpatch.exe unpatch resources\smaller2bigger_patched.txt resources\smaller2bigger_patch.sp resources\smaller2bigger_unpatched.txt
 
-@del resources\bigger2smaller_patch.sp 2>NUL
-@del resources\bigger2smaller_patched.txt 2>NUL
+del resources\bigger2smaller_patch.sp 2>NUL
+del resources\bigger2smaller_patched.txt 2>NUL
+del resources\bigger2smaller_unpatched.txt 2>NUL
 
 zig-out\bin\Starpatch.exe create resources\bigger.txt resources\smaller.txt resources\bigger2smaller_patch.sp
 zig-out\bin\Starpatch.exe patch resources\bigger.txt resources\bigger2smaller_patch.sp resources\bigger2smaller_patched.txt
+zig-out\bin\Starpatch.exe unpatch resources\bigger2smaller_patched.txt resources\bigger2smaller_patch.sp resources\bigger2smaller_unpatched.txt
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std: type = @import("std");
 const rl: type = @import("raylib");
 
+const STARPATCH_VERSION: u8 = 1;
+
 const PatchType: type = enum(u8) {
     add,
     delete,
@@ -8,10 +10,15 @@ const PatchType: type = enum(u8) {
 };
 
 const PatchError: type = error{
-    SaveFailed,
+    OriginalCRC32Mismatched,
+    OriginalFileSizeMismatched,
+    PatchedCRC32Mismatched,
+    PatchedFileSizeMismatched,
     ReadBufferFailed,
-    CRC32Mismatched,
-    FileSizeMismatched,
+    SaveFailed,
+    WrongByte,
+    WrongHeader,
+    WrongVersion,
 };
 
 pub fn read8bits(buffer: *const []u8, i: *u32) anyerror!u8 {
@@ -59,13 +66,21 @@ pub fn createPatch(allocator: std.mem.Allocator, originalFileName: [*:0]const u8
     const patchedData: []u8 = try rl.loadFileData(patchedFileName);
     defer rl.unloadFileData(patchedData);
 
-    const originalCrc: u32 = std.hash.crc.Crc32.hash(originalData);
-
     var buffer: std.ArrayListAligned(u8, null) = std.ArrayList(u8).init(allocator);
     defer buffer.deinit();
 
-    try write32bits(&buffer, originalCrc);
+    _ = try buffer.writer().write("STARPATCH");
+
+    try buffer.append(STARPATCH_VERSION);
+
     try write32bits(&buffer, @as(u32, @intCast(originalData.len)));
+    try write32bits(&buffer, @as(u32, @intCast(patchedData.len)));
+
+    const originalCrc: u32 = std.hash.crc.Crc32.hash(originalData);
+    const patchedCrc: u32 = std.hash.crc.Crc32.hash(patchedData);
+
+    try write32bits(&buffer, originalCrc);
+    try write32bits(&buffer, patchedCrc);
 
     var i: u32 = 0;
 
@@ -80,6 +95,7 @@ pub fn createPatch(allocator: std.mem.Allocator, originalFileName: [*:0]const u8
 
                 try buffer.append(@intFromEnum(PatchType.modify));
 
+                try buffer.append(byteOriginal);
                 try buffer.append(bytePatched);
             }
         } else {
@@ -87,7 +103,7 @@ pub fn createPatch(allocator: std.mem.Allocator, originalFileName: [*:0]const u8
 
             try buffer.append(@intFromEnum(PatchType.delete));
 
-            break;
+            try buffer.append(byteOriginal);
         }
 
         i += 1;
@@ -118,18 +134,35 @@ pub fn applyPatch(allocator: std.mem.Allocator, originalFileName: [*:0]const u8,
 
     var i: u32 = 0;
 
-    const originalCrc: u32 = std.hash.crc.Crc32.hash(originalData);
-    const patchCrc: u32 = try read32bits(&patchData, &i);
+    var header: [10]u8 = .{0} ** 10;
 
-    if (originalCrc != patchCrc) {
-        return PatchError.CRC32Mismatched;
+    while (i < header.len) {
+        header[i] = try read8bits(&patchData, &i);
+    }
+
+    if (std.mem.eql(u8, header[0..10], "STARPATCH")) {
+        return PatchError.WrongHeader;
+    }
+
+    if (header[header.len - 1] != STARPATCH_VERSION) {
+        return PatchError.WrongVersion;
     }
 
     const originalSize: u32 = try read32bits(&patchData, &i);
+    const patchedSize: u32 = try read32bits(&patchData, &i);
 
     if (originalData.len != originalSize) {
-        return PatchError.FileSizeMismatched;
+        return PatchError.OriginalFileSizeMismatched;
     }
+
+    const originalCrc: u32 = std.hash.crc.Crc32.hash(originalData);
+    const originalCrcInPatch: u32 = try read32bits(&patchData, &i);
+
+    if (originalCrc != originalCrcInPatch) {
+        return PatchError.OriginalCRC32Mismatched;
+    }
+
+    const patchedCrcInPatch: u32 = try read32bits(&patchData, &i);
 
     var patchedData: std.ArrayListAligned(u8, null) = std.ArrayList(u8).init(allocator);
     defer patchedData.deinit();
@@ -138,24 +171,133 @@ pub fn applyPatch(allocator: std.mem.Allocator, originalFileName: [*:0]const u8,
 
     @memcpy(patchedData.items, originalData);
 
-    while (i < patchData.len) {
-        const address: u32 = try read32bits(&patchData, &i);
-        const action: PatchType = @as(PatchType, @enumFromInt(try read8bits(&patchData, &i)));
+    applyPatch: {
+        while (i < patchData.len) {
+            const address: u32 = try read32bits(&patchData, &i);
+            const action: PatchType = @as(PatchType, @enumFromInt(try read8bits(&patchData, &i)));
 
-        switch (action) {
-            .add => {
-                try patchedData.append(try read8bits(&patchData, &i));
-            },
-            .delete => {
-                patchedData.shrinkAndFree(address);
-            },
-            .modify => {
-                patchedData.items[address] = try read8bits(&patchData, &i);
-            },
+            switch (action) {
+                .add => {
+                    try patchedData.append(try read8bits(&patchData, &i));
+                },
+                .delete => {
+                    patchedData.shrinkAndFree(address);
+
+                    break :applyPatch;
+                },
+                .modify => {
+                    const originalByte: u8 = try read8bits(&patchData, &i);
+
+                    if (patchedData.items[address] != originalByte) {
+                        return PatchError.WrongByte;
+                    }
+
+                    patchedData.items[address] = try read8bits(&patchData, &i);
+                },
+            }
         }
     }
 
+    if (patchedData.items.len != patchedSize) {
+        return PatchError.PatchedFileSizeMismatched;
+    }
+
+    const patchedCrc: u32 = std.hash.crc.Crc32.hash(patchedData.items);
+
+    if (patchedCrc != patchedCrcInPatch) {
+        return PatchError.PatchedCRC32Mismatched;
+    }
+
     if (!rl.saveFileData(patchedFileName, patchedData.items)) {
+        return PatchError.SaveFailed;
+    }
+}
+
+pub fn applyUnpatch(allocator: std.mem.Allocator, patchedFileName: [*:0]const u8, patchFileName: [*:0]const u8, originalFileName: [*:0]const u8) anyerror!void {
+    const patchedData: []u8 = try rl.loadFileData(patchedFileName);
+    defer rl.unloadFileData(patchedData);
+
+    const patchData: []u8 = try rl.loadFileData(patchFileName);
+    defer rl.unloadFileData(patchData);
+
+    var i: u32 = 0;
+
+    var header: [10]u8 = .{0} ** 10;
+
+    while (i < header.len) {
+        header[i] = try read8bits(&patchData, &i);
+    }
+
+    if (std.mem.eql(u8, header[0..10], "STARPATCH")) {
+        return PatchError.WrongHeader;
+    }
+
+    if (header[header.len - 1] != STARPATCH_VERSION) {
+        return PatchError.WrongVersion;
+    }
+
+    const originalSize: u32 = try read32bits(&patchData, &i);
+    const patchedSize: u32 = try read32bits(&patchData, &i);
+
+    if (patchedData.len != patchedSize) {
+        return PatchError.PatchedFileSizeMismatched;
+    }
+
+    const originalCrcInPatch: u32 = try read32bits(&patchData, &i);
+
+    const patchedCrc: u32 = std.hash.crc.Crc32.hash(patchedData);
+    const patchedCrcInPatch: u32 = try read32bits(&patchData, &i);
+
+    if (patchedCrc != patchedCrcInPatch) {
+        return PatchError.PatchedCRC32Mismatched;
+    }
+
+    var originalData: std.ArrayListAligned(u8, null) = std.ArrayList(u8).init(allocator);
+    defer originalData.deinit();
+
+    try originalData.resize(patchedData.len);
+
+    @memcpy(originalData.items, patchedData);
+
+    applyUnpatch: {
+        while (i < patchData.len) {
+            const address: u32 = try read32bits(&patchData, &i);
+            const action: PatchType = @as(PatchType, @enumFromInt(try read8bits(&patchData, &i)));
+
+            switch (action) {
+                .add => {
+                    originalData.shrinkAndFree(address);
+
+                    break :applyUnpatch;
+                },
+                .delete => {
+                    try originalData.append(try read8bits(&patchData, &i));
+                },
+                .modify => {
+                    const originalByte: u8 = try read8bits(&patchData, &i);
+                    const patchedByte: u8 = try read8bits(&patchData, &i);
+
+                    if (originalData.items[address] != patchedByte) {
+                        return PatchError.WrongByte;
+                    }
+
+                    originalData.items[address] = originalByte;
+                },
+            }
+        }
+    }
+
+    if (originalData.items.len != originalSize) {
+        return PatchError.OriginalFileSizeMismatched;
+    }
+
+    const originalCrc: u32 = std.hash.crc.Crc32.hash(originalData.items);
+
+    if (originalCrc != originalCrcInPatch) {
+        return PatchError.OriginalCRC32Mismatched;
+    }
+
+    if (!rl.saveFileData(originalFileName, originalData.items)) {
         return PatchError.SaveFailed;
     }
 }
@@ -165,8 +307,9 @@ pub fn help(args: [][:0]u8) anyerror!void {
 
     _ = try std.io.getStdOut().write("Usage:\n");
 
-    try std.io.getStdOut().writer().print("\t{s} create <original file> <patched file> <output patch file>\n", .{fileName});
-    try std.io.getStdOut().writer().print("\t{s} patch  <original file> <patch file> <output patched file>\n", .{fileName});
+    try std.io.getStdOut().writer().print("\t{s} create  <original file> <patched file> <output patch file>\n", .{fileName});
+    try std.io.getStdOut().writer().print("\t{s} patch   <original file> <patch file> <output patched file>\n", .{fileName});
+    try std.io.getStdOut().writer().print("\t{s} unpatch <patched file> <patch file> <output original file>\n", .{fileName});
 }
 
 pub fn main() anyerror!u8 {
@@ -186,8 +329,9 @@ pub fn main() anyerror!u8 {
 
     const isCreate: bool = std.mem.eql(u8, args[1], "create");
     const isPatch: bool = std.mem.eql(u8, args[1], "patch");
+    const isUnpatch: bool = std.mem.eql(u8, args[1], "unpatch");
 
-    if (!isCreate and !isPatch) {
+    if (!isCreate and !isPatch and !isUnpatch) {
         try help(args);
 
         return 1;
@@ -200,7 +344,7 @@ pub fn main() anyerror!u8 {
     }
 
     if (rl.isPathFile(args[4])) {
-        _ = try std.io.getStdOut().write("Output file already exists\n");
+        _ = try std.io.getStdErr().write("Output file already exists\n");
 
         return 1;
     }
@@ -208,9 +352,74 @@ pub fn main() anyerror!u8 {
     rl.setTraceLogLevel(.err);
 
     if (isCreate) {
-        try createPatch(gpa_allocator, args[2], args[3], args[4]);
+        createPatch(gpa_allocator, args[2], args[3], args[4]) catch |createError| switch (createError) {
+            PatchError.SaveFailed => {
+                _ = try std.io.getStdErr().write("Error saving patch file\n");
+            },
+            else => {
+                _ = try std.io.getStdErr().writer().print("Error while creating patch: {any}\n", .{createError});
+            },
+        };
     } else if (isPatch) {
-        try applyPatch(gpa_allocator, args[2], args[3], args[4]);
+        applyPatch(gpa_allocator, args[2], args[3], args[4]) catch |patchError| switch (patchError) {
+            PatchError.OriginalCRC32Mismatched => {
+                _ = try std.io.getStdErr().write("Original file's CRC32 does not match the patch's expected CRC32\n");
+            },
+            PatchError.OriginalFileSizeMismatched => {
+                _ = try std.io.getStdErr().write("Original file's size does not match the patch's expected file size\n");
+            },
+            PatchError.PatchedCRC32Mismatched => {
+                _ = try std.io.getStdErr().write("Patched file's CRC32 does not match the patch's expected CRC32\n");
+            },
+            PatchError.PatchedFileSizeMismatched => {
+                _ = try std.io.getStdErr().write("Patched file's size does not match the patch's expected file size\n");
+            },
+            PatchError.SaveFailed => {
+                _ = try std.io.getStdErr().write("Error saving patched file\n");
+            },
+            PatchError.WrongByte => {
+                _ = try std.io.getStdErr().write("Wrong original byte found while applying the patch\n");
+            },
+            PatchError.WrongHeader => {
+                _ = try std.io.getStdErr().write("Patch file has wrong header\n");
+            },
+            PatchError.WrongVersion => {
+                _ = try std.io.getStdErr().write("Patch file has wrong version\n");
+            },
+            else => {
+                _ = try std.io.getStdErr().writer().print("Error while applying patch: {any}\n", .{patchError});
+            },
+        };
+    } else if (isUnpatch) {
+        applyUnpatch(gpa_allocator, args[2], args[3], args[4]) catch |patchError| switch (patchError) {
+            PatchError.OriginalCRC32Mismatched => {
+                _ = try std.io.getStdErr().write("Original file's CRC32 does not match the patch's expected CRC32\n");
+            },
+            PatchError.OriginalFileSizeMismatched => {
+                _ = try std.io.getStdErr().write("Original file's size does not match the patch's expected file size\n");
+            },
+            PatchError.PatchedCRC32Mismatched => {
+                _ = try std.io.getStdErr().write("Patched file's CRC32 does not match the patch's expected CRC32\n");
+            },
+            PatchError.PatchedFileSizeMismatched => {
+                _ = try std.io.getStdErr().write("Patched file's size does not match the patch's expected file size\n");
+            },
+            PatchError.SaveFailed => {
+                _ = try std.io.getStdErr().write("Error saving unpatched file\n");
+            },
+            PatchError.WrongByte => {
+                _ = try std.io.getStdErr().write("Wrong patched byte found while applying the unpatch\n");
+            },
+            PatchError.WrongHeader => {
+                _ = try std.io.getStdErr().write("Patch file has wrong header\n");
+            },
+            PatchError.WrongVersion => {
+                _ = try std.io.getStdErr().write("Patch file has wrong version\n");
+            },
+            else => {
+                _ = try std.io.getStdErr().writer().print("Error while applying unpatch: {any}\n", .{patchError});
+            },
+        };
     } else {
         unreachable;
     }
